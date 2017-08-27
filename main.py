e30fd58fbe6cc1e88f6f328b54e0322ae14b741f
Will-C-Hopper <31353207+Will-C-Hopper@users.noreply.github.com>
2017-08-27 14:07:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,25 @@
+from flask import Flask, session, redirect, url_for, escape, request
 
+app = Flask(__name__)
+
+@app.route('/request')
+def request():
+  
+@app.route('/view')
+def view():
+  
+
+@app.route('/')
+def index():
+    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    
+
+@app.route('/logout')
+def logout():
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
